64ac6f76233e34842c934ca147aeeea73b212f7f
Sara Zhou <sara@Saras-MacBook-Air.local>
2024-04-17 21:49:45 +1000

    add js code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,103 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT License": return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+      break;
+    case "No License": return "";
+      break;
+    case "Apache 2.0 License": return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case "GNU GPL v3": return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case "BSD 3-Clause License": return `[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+      break;
+    case "BSD 2-Clause License": return `[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)`;
+      break;
+    case "Boost Software License 1.0": return `[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)`;
+      break;
+    case "Eclipse Public License 1.0": return `[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)`;
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT License": return `(https://opensource.org/licenses/MIT)`;
+      break;
+    case "No License": return "";
+      break;
+    case "Apache 2.0 License": return `(https://opensource.org/licenses/Apache-2.0)`;
+      break;
+    case "GNU GPL v3": return `(https://www.gnu.org/licenses/gpl-3.0)`;
+      break;
+    case "BSD 3-Clause License": return `(https://opensource.org/licenses/BSD-3-Clause)`;
+      break;
+    case "BSD 2-Clause License": return `(https://opensource.org/licenses/BSD-2-Clause)`;
+      break;
+    case "Boost Software License 1.0": return `(https://www.boost.org/LICENSE_1_0.txt)`;
+      break;
+    case "Eclipse Public License 1.0": return `(https://opensource.org/licenses/EPL-1.0)`;
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title} ${renderLicenseBadge(data.license)}
 
+## Description
+
+${data.description}
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Test](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+This application is covered under ${data.license}${renderLicenseLink(data.license)}.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## Contributing
+
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg)](code_of_conduct.md)
+
+https://www.contributor-covenant.org/
+
+## Tests
+
+Then provide examples on how to run tests here.
+
+## Questions
+
+Contact this email <${data.email}> with additional questions
+
+[Link ](https://github.com/${data.github}) to Github Profile
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = { generateMarkdown };
--- README.md
+++ README.md
@@ -1,21 +1,16 @@
 # Professional README Generator
 
-## Your Task
+## Description
 
-When creating an open source project on GitHub, it’s important to have a high-quality README for the app. This should include what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions&mdash;this last part increases the likelihood that other developers will contribute to the success of the project. 
+When creating an open source project on GitHub, it’s important to have a high-quality README for the app. 
 
-You can quickly and easily create a README file by using a command-line application to generate one. This allows the project creator to devote more time to working on the project.
-
-Your task is to create a command-line application that dynamically generates a professional README.md file from a user's input using the [Inquirer package](https://www.npmjs.com/package/inquirer/v/8.2.4). Review the [Professional README Guide](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide) as a reminder of everything that a high-quality, professional README should contain. 
+This command-line application dynamically generates a professional README.md file from a user's input using the [Inquirer package](https://www.npmjs.com/package/inquirer/v/8.2.4). 
 
 The application will be invoked by using the following command:
 
 ```bash
 node index.js
 ```
-
-Because this application won’t be deployed, you’ll also need to provide a link to a walkthrough video that demonstrates its functionality. Revisit the Screencastify Tutorial in the prework as a refresher on how to record video from your computer. You’ll need to submit a link to the video _and_ add it to the README of your project.
-
 
 ## User Story
 
@@ -45,23 +40,13 @@ WHEN I click on the links in the Table of Contents
 THEN I am taken to the corresponding section of the README
 ```
 
-## Getting Started
+## Usage
 
-Here are some guidelines to help you get started:
+The following image shows the web application's appearance and functionality:
 
-* Create a `.gitignore` file and include `node_modules/` and `.DS_Store/` so that your `node_modules` directory isn't tracked or uploaded to GitHub. Be sure to create your `.gitignore` file before installing any npm dependencies.
-
-* Make sure that your repo includes a `package.json` with the required dependencies. You can create one by running `npm init` when you first set up the project, before installing any dependencies.
-
-* Include a video of the typical user flow through your application. This includes views of the prompts and the responses after their selection.
-
-* Refer to the [Fullstack Blog Video Submission Guide](https://coding-boot-camp.github.io/full-stack/computer-literacy/video-submission-guide) for additional guidance on creating a video.
-
-* Include any other screenshots you deem necessary to help someone who has never been introduced to your application understand the purpose and function of it. This is how you will communicate to potential employers or other developers in the future what you built and why, and to show how it works.
+[![screenshot]()]
 
 ## Review
-
-You are required to submit the following for review:
 
 * A walkthrough video demonstrating the functionality of the application.
 
